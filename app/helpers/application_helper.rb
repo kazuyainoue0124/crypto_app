c9ae496d6491cad710ba994cd9e79521a97f6363
kazuyainoue <bauer24tbp@gmail.com>
2022-05-02 15:17:52 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # ページ毎の完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "Crypto App for Portfolio"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
