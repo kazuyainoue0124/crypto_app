6a34578115437089b59c4a5593703d46fb9d7e18
kazuyainoue <bauer24tbp@gmail.com>
2022-05-05 22:05:41 +0900

    Add bcrypt to Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,14 @@ ActiveRecord::Schema.define(version: 2022_05_05_123257) do
     t.text "url"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "profile"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
