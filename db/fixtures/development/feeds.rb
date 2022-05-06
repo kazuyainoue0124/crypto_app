2ace66f91fcb60bc0c287227662e05f500cae644
kazuyainoue <bauer24tbp@gmail.com>
2022-05-06 15:10:28 +0900

    Add coinpost and cointelegraph for rss

--- db/fixtures/development/feeds.rb
+++ db/fixtures/development/feeds.rb
@@ -3,3 +3,15 @@ Feed.seed do |s|
   s.rss_url ="https://www.coindeskjapan.com/rss"
   s.url = "https://www.coindeskjapan.com/"
 end
+
+Feed.seed do |s|
+  s.name ="coinpost"
+  s.rss_url ="https://coinpost.jp/rss.xml"
+  s.url = "https://coinpost.jp/"
+end
+
+Feed.seed do |s|
+  s.name ="cointelegraph"
+  s.rss_url ="https://jp.cointelegraph.com/rss"
+  s.url = "https://jp.cointelegraph.com/"
+end
