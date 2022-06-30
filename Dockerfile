c55d3f719ab46f5109bcc0ca5da11ab5af46af96
kazuyainoue <bauer24tbp@gmail.com>
2022-06-30 21:42:29 +0900

    docker環境で最新の記事が自動取得されるよう修正しました #30

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ruby:3.1.2
 
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs default-mysql-client yarn
-
+RUN apt-get install -y cron
 RUN mkdir /workdir
 WORKDIR /workdir
 
@@ -16,4 +16,6 @@ RUN chmod +x /usr/bin/entrypoint.sh
 ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
 
+RUN bundle exec whenever --update-crontab
+CMD ["cron", "-f"]
 CMD ["rails", "server", "-b", "0.0.0.0"]
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,7 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+ENV.each { |k, v| env(k, v) } # 環境変数をwheneverにいい感じに書き込んでもらう
+set :output, error: 'log/crontab_error.log', standard: 'log/crontab.log' # ログを書き出すようにしておくとデバッグが楽
+set :environment, :development
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-set :output, 'log/crontab.log'
-set :environment, ENV['RAILS_ENV']
-
-every 1.hour do
+every 1.minutes do
   rake "rss:feeds"
 end
